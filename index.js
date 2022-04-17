568a24528932c0e56a1e6afc722b7477647748c0
rajamoris <raja.moris@spenmo.com>
2022-04-17 10:39:45 +0700

    add docker-compose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Init Test</h2>");
+  res.send("<h2>Init My Node Project</h2>");
 });
 
 const port = process.env.PORT || 3000;
